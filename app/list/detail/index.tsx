a5c3618a945dfbc77bf0b9086eea9c7e046672f3
dylanXM <dylan_xm@163.com>
2024-09-16 23:51:48 +0800

    fix: any 类型替换

--- app/list/detail/index.tsx
+++ app/list/detail/index.tsx
@@ -14,12 +14,19 @@ import ActivityTab from './components/activity-tab';
 import { color_complement, color_primary, color_primary_dark } from '@/constants/Colors';
 import React from 'react';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default function Detail({ route }: { route: any }) {
+interface DetailProps {
+  route: {
+    params: {
+      activity: Twitter;
+    };
+  };
+}
+
+export default function Detail({ route }: DetailProps) {
   const { user } = useSelector((state: RootState) => state.user);
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const navigation = useNavigation<NavigationProp<any>>();
-  const { activity } = route.params as { activity: Twitter };
+  const { activity } = route.params;
   const dispatch = useDispatch();
   const [updateKey, setUpdateKey] = useState(0);
 
